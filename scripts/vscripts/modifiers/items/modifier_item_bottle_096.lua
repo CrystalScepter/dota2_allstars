51396f1be308e7bf231bcf6956941afa0cccd7c8
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2024-05-12 22:53:20 -0300

    Adds Flask of Sapphire Water item

---
+++ scripts/vscripts/modifiers/items/modifier_item_bottle_096.lua
@@ -0,0 +1,35 @@
+modifier_item_bottle_096 = class({})
+
+-- Called when the modifier is created
+function modifier_item_bottle_096:OnCreated( kv )
+	self.heal = self:GetAbility():GetSpecialValueFor( "health_restore" )
+	self.duration = self:GetAbility():GetSpecialValueFor( "duration" )
+	self.interval = self:GetAbility():GetSpecialValueFor( "heal_interval" )
+	
+	if IsServer() then
+		self:SetDuration( self.duration, false )
+		
+		-- Start the 1 second interval for healing
+		self:StartIntervalThink( self.interval )
+	end
+end
+
+-- Applies the modifier effect every second
+function modifier_item_bottle_096:OnIntervalThink()
+	
+	-- Calculates the heal amount per second
+	local heal_amount = self.heal / self.duration
+	
+	-- Heals the target that the item was cast upon
+	self:GetParent():Heal( heal_amount, self:GetAbility() )
+end
+
+-- Makes the modifier dispellable
+function modifier_item_bottle_096:IsPurgable()
+	return true
+end
+
+-- Returns the texture to be displayed in the buff bar in case the item runs out of charges
+function modifier_item_bottle_096:GetTexture()
+	return "item_bottle"
+end
