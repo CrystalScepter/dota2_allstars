97069bebf6fc846750b1d37127b154e5cd2d6adf
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2024-05-16 19:38:02 -0300

    Starting work on heroes

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,4 +1,9 @@
--- Precaches the assets that are going to be used in the custom game
+-- Creates the global game mode class
+if dota2_096 == nil then
+	dota2_096 = class({})
+end
+
+-- Precaches the assets that are going to be used in the custom game 
 function Precache( context )
 	PrecacheResource( "soundfile", "soundevents/game_sounds_heroes/game_sounds_rattletrap.vsndevts", context )
 end
@@ -8,15 +13,15 @@ function Activate()
 	
 	-- Commands with the intention of speeding up the process of getting into the game, for testing purposes while the addon is still in development
 	-- Defaults to Radiant Side and goes into the hero selection immediately, jumps straight into the game after the hero has been chosen
-	GameRules:EnableCustomGameSetupAutoLaunch(true)
-	GameRules:SetCustomGameSetupAutoLaunchDelay(0)
-    	GameRules:SetHeroSelectionTime(10)
-	GameRules:SetStrategyTime(0)
-	GameRules:SetShowcaseTime(0)
+	GameRules:EnableCustomGameSetupAutoLaunch( true )
+	GameRules:SetCustomGameSetupAutoLaunchDelay( 0 )
+    	GameRules:SetHeroSelectionTime( 10 )
+	GameRules:SetStrategyTime( 0 )
+	GameRules:SetShowcaseTime( 0 )
 	
-	-- Allows you to purchase items quickly for testing purposes
-	GameRules:SetStartingGold(75000)
+	-- Sets the correct starting gold
+	GameRules:SetStartingGold( 500 )
 	
 	-- Enable sandbox commands in DotA for testing purposes
-	SendToConsole ( "sv_cheats 1" )
+	SendToConsole( "sv_cheats 1" )
 end
