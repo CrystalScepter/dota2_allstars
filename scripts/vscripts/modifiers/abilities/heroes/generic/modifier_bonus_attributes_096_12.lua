8dd019a67691689fba4673ca28a65bb429e2c82c
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2024-05-27 17:18:07 -0300

    Creates the bonus attribute ability
    Has a limit of 12 levels

---
+++ scripts/vscripts/modifiers/abilities/heroes/generic/modifier_bonus_attributes_096_12.lua
@@ -0,0 +1,45 @@
+modifier_bonus_attributes_096_12 = class({})
+
+-- Called when the modifier is created
+function modifier_bonus_attributes_096_12:OnCreated( kv )
+	self.bonus_strength = self:GetAbility():GetSpecialValueFor( "all_attributes" )
+	self.bonus_agility = self:GetAbility():GetSpecialValueFor( "all_attributes" )
+	self.bonus_intelligence = self:GetAbility():GetSpecialValueFor( "all_attributes" )
+end
+
+-- Called when the modifier is refreshed
+function modifier_bonus_attributes_096_12:OnRefresh( kv )
+	self.bonus_strength = self:GetAbility():GetSpecialValueFor( "all_attributes" )
+	self.bonus_agility = self:GetAbility():GetSpecialValueFor( "all_attributes" )
+	self.bonus_intelligence = self:GetAbility():GetSpecialValueFor( "all_attributes" )
+end
+
+-- Returns the events and properties our modifier affects
+function modifier_bonus_attributes_096_12:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+	}
+	return funcs
+end
+
+-- Returns the value for the property
+function modifier_bonus_attributes_096_12:GetModifierBonusStats_Strength( params )
+	return self.bonus_strength
+end
+
+-- Returns the value for the property
+function modifier_bonus_attributes_096_12:GetModifierBonusStats_Agility( params )
+	return self.bonus_agility
+end
+
+-- Returns the value for the property
+function modifier_bonus_attributes_096_12:GetModifierBonusStats_Intellect( params )
+	return self.bonus_intelligence
+end
+
+-- Prevents the modifier from showing up on the buff bar
+function modifier_bonus_attributes_096_12:IsHidden()
+	return true
+end
