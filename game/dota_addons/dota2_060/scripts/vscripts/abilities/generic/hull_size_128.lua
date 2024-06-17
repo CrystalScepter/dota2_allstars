be75c52ecf7c24d41cc3985aa47f65af314506d7
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2024-06-17 17:51:36 -0300

    Adds filler buildings to the Radiant base
    Also added a hitbox to the fountain model, making it selectable

---
+++ game/dota_addons/dota2_060/scripts/vscripts/abilities/generic/hull_size_128.lua
@@ -0,0 +1,9 @@
+hull_size_128 = class({})
+
+-- Sets the ability level to 1 when it's created and the hull size of the unit to 128
+function hull_size_128:Spawn()
+	if IsServer() then
+		self:SetLevel(1)
+		self:GetCaster():SetHullRadius(128)
+	end
+end
--- game/dota_addons/dota2_060/scripts/vscripts/events.lua
+++ game/dota_addons/dota2_060/scripts/vscripts/events.lua
@@ -8,14 +8,38 @@ function GameMode:OnGameRulesStateChange(keys)
 		-- Adds ancients onto the map
 		local point_ancient_radiant = Entities:FindByName(nil, "npc_dota_ancient_radiant"):GetAbsOrigin()
 		local point_ancient_dire = Entities:FindByName(nil, "npc_dota_ancient_dire"):GetAbsOrigin()
-		local ancient_radiant = CreateUnitByName("npc_dota_ancient_radiant_060", point_ancient_radiant, false, nil, nil, DOTA_TEAM_GOODGUYS)
-		local ancient_dire = CreateUnitByName("npc_dota_ancient_dire_060", point_ancient_dire, false, nil, nil, DOTA_TEAM_BADGUYS)
+		CreateUnitByName("npc_dota_ancient_radiant_060", point_ancient_radiant, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_ancient_dire_060", point_ancient_dire, false, nil, nil, DOTA_TEAM_BADGUYS)
 
 		-- Adds fountains onto the map
 		local point_fountain_radiant = Entities:FindByName(nil, "npc_dota_fountain_radiant"):GetAbsOrigin()
 		local point_fountain_dire = Entities:FindByName(nil, "npc_dota_fountain_dire"):GetAbsOrigin()
-		local fountain_radiant = CreateUnitByName("npc_dota_fountain_radiant_060", point_fountain_radiant, false, nil, nil, DOTA_TEAM_GOODGUYS)
-		local fountain_dire = CreateUnitByName("npc_dota_fountain_dire_060", point_fountain_dire, false, nil, nil, DOTA_TEAM_BADGUYS)
+		CreateUnitByName("npc_dota_fountain_radiant_060", point_fountain_radiant, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_fountain_dire_060", point_fountain_dire, false, nil, nil, DOTA_TEAM_BADGUYS)
+
+		-- Adds filler buildings onto the map
+		local point_filler_radiant_1 = Entities:FindByName(nil, "npc_dota_filler_radiant_1"):GetAbsOrigin()
+		local point_filler_radiant_2 = Entities:FindByName(nil, "npc_dota_filler_radiant_2"):GetAbsOrigin()
+		local point_filler_radiant_3 = Entities:FindByName(nil, "npc_dota_filler_radiant_3"):GetAbsOrigin()
+		local point_filler_radiant_4 = Entities:FindByName(nil, "npc_dota_filler_radiant_4"):GetAbsOrigin()
+		local point_filler_radiant_5 = Entities:FindByName(nil, "npc_dota_filler_radiant_5"):GetAbsOrigin()
+		local point_filler_radiant_6 = Entities:FindByName(nil, "npc_dota_filler_radiant_6"):GetAbsOrigin()
+		local point_filler_radiant_7 = Entities:FindByName(nil, "npc_dota_filler_radiant_7"):GetAbsOrigin()
+		local point_filler_radiant_8 = Entities:FindByName(nil, "npc_dota_filler_radiant_8"):GetAbsOrigin()
+		local point_filler_radiant_9 = Entities:FindByName(nil, "npc_dota_filler_radiant_9"):GetAbsOrigin()
+		local point_filler_radiant_10 = Entities:FindByName(nil, "npc_dota_filler_radiant_10"):GetAbsOrigin()
+		local point_filler_radiant_11 = Entities:FindByName(nil, "npc_dota_filler_radiant_11"):GetAbsOrigin()
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_1, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_2, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_3, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_4, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_5, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_6, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_7, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_8, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060", point_filler_radiant_9, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060_2", point_filler_radiant_10, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		CreateUnitByName("npc_dota_filler_radiant_060_2", point_filler_radiant_11, false, nil, nil, DOTA_TEAM_GOODGUYS)
 	end
 end
 
